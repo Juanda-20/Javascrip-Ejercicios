8a77bc3204e74ed1f4b56f4861c5ffb0915eecf0
xIsaaza <eisaza47@misena.edu.co>
2023-02-21 07:25:24 -0500

    first commit

---
+++ Grados.js
@@ -0,0 +1,8 @@
+
+    
+	var centi, fare;
+
+	centi = parseFloat(prompt("Ingresa grados centígrados"));
+	fare = (9/5*centi)+32;
+
+	console.log(centi+" centígrados equivalen a "+fare+" farenheit");
---
+++ Notas.js
@@ -0,0 +1,7 @@
+var Nota1 = parseFloat(prompt("Digita la Nota 1"))
+var Nota2 = parseFloat(prompt("Digita la Nota 2"))
+var Nota3 = parseFloat(prompt("Digita la Nota 3"))
+var promedio = 0
+
+promedio = (Nota1 + Nota2 + Nota3)/3
+console.log("el promedio de nota es:"+ promedio)
